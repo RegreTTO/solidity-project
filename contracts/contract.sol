26944a2e8d4223337cfea42855696b9d0c229e74
RegreTTO <atary_game@mail.ru>
2023-02-16 23:50:10 +0300

    added fundraising struct and opening new fundraisings

---
+++ contracts/contract.sol
@@ -0,0 +1,38 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity 0.8.15;
+
+struct Fundraising {
+	uint256 id;
+	string description;
+	uint256 goal; // etherium aim
+	uint256 current;
+	address owner;
+	bool opened;
+}
+
+contract Crowdfunding {
+	address contract_owner;
+	Fundraising[] public fundraisings;
+	mapping(uint256 => mapping(address => uint256)) public contributions;
+
+	event fundraisingOpenEvent(uint256 id, address owner, string description, uint256 goal);
+
+	constructor() {
+		contract_owner = msg.sender;
+	}
+
+	function openNewFundraising(uint256 _goal, string calldata _description) public {
+		require(bytes(_description).length != 0);
+        require(_goal != 0);
+		Fundraising memory fund = Fundraising({
+			id: fundraisings.length + 1,
+			description: _description,
+			goal: _goal,
+			current: 0,
+			owner: msg.sender,
+			opened: true
+		});
+		fundraisings.push(fund);
+        emit fundraisingOpenEvent(fund.id, fund.owner, fund.description, fund.goal);
+	}
+}
