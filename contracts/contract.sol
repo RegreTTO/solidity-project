783a08b789db30f44782f679c3769bc6c278cbbf
RegreTTO <atary_game@mail.ru>
2023-02-17 14:53:01 +0300

    donation added

--- contracts/contract.sol
+++ contracts/contract.sol
@@ -7,24 +7,36 @@ struct Fundraising {
 	uint256 goal; // etherium aim
 	uint256 current;
 	address owner;
-	uint256 closing_time;
+	uint256 closingTime;
 	bool opened;
 }
 
 contract Crowdfunding {
-	address contract_owner;
+	address contractOwner;
+
 	Fundraising[] public fundraisings;
-	mapping(uint256 => mapping(address => uint256)) public contributions;
+	
+	mapping(uint256 => mapping(address => uint256)) public contributions; // fundraising id, user-address, funding
 
-	event fundraisingOpenEvent(uint256 id, address owner, string description, uint256 goal);
+	event FundraisingOpenEvent(uint256 id, address owner, string description, uint256 goal, uint256 closingTime);
+	event DonationAccepted(uint256 fundraisingId, uint256 value);
 
-	constructor() {
-		contract_owner = msg.sender;
+	constructor() payable {
+		contractOwner = msg.sender;
 	}
 
-	function openNewFundraising(uint256 _goal, string calldata _description, uint256 _closing_time) public {
+	receive() external payable {
+	}
+
+	fallback () external payable {}
+
+	function openNewFundraising(
+		uint256 _goal,
+		string calldata _description,
+		uint256 _closingTime
+	) public {
 		require(bytes(_description).length != 0);
-        require(_goal != 0);
+		require(_goal != 0);
 
 		Fundraising memory fund = Fundraising({
 			id: fundraisings.length + 1,
@@ -33,11 +45,18 @@ contract Crowdfunding {
 			current: 0,
 			owner: msg.sender,
 			opened: true,
-			closing_time: _closing_time
+			closingTime: block.timestamp + _closingTime
 		});
-		
+
 		fundraisings.push(fund);
-        emit fundraisingOpenEvent(fund.id, fund.owner, fund.description, fund.goal);
+		emit FundraisingOpenEvent(fund.id, fund.owner, fund.description, fund.goal, fund.closingTime);
 	}
 
+	function donate(uint256 _id) public payable {
+		require(msg.value != 0, "Wei count must be not null!");
+		fundraisings[_id].current += msg.value;
+		contributions[_id][msg.sender] += msg.value;
+
+		emit DonationAccepted(_id, msg.value);
+	}
 }
